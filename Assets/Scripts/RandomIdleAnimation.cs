ce7e9fd7cdab78c1c739b317c58827b15a70a180
Bruno Barreiro Bastos Arnaud Fonseca <bruno.arnaud.br@gmail.com>
2025-08-06 17:06:13 -0300

    add idle animation and random animation start

---
+++ Assets/Scripts/RandomIdleAnimation.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class RandomIdleAnimation : MonoBehaviour
+{
+    [SerializeField] Animator myAnimator;
+
+    private void Awake()
+    {
+        myAnimator = GetComponent<Animator>();
+    }
+
+    void Start()
+    {
+        if (!myAnimator) return;
+
+        AnimatorStateInfo state = myAnimator.GetCurrentAnimatorStateInfo(0);
+        myAnimator.Play(state.fullPathHash, -1, Random.Range(0, 1f));
+    }
+}
